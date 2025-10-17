aec063359bcae57cdc9e04c3c595653836758cc0
new_pointer <new_chenyiliang@outlook.com>
2025-10-18 03:24:17 +0800

    修复一些问题

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
 #include <QSystemTrayIcon>
+#include <QMessageBox>
 #include "mainwindow.h"
+#include "windowstraymanager.h"
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +11,12 @@ int main(int argc, char* argv[])
     // 检查系统托盘是否可用
     if (!QSystemTrayIcon::isSystemTrayAvailable()) {
         qCritical() << "System tray is not available on this system.";
+        return 1;
+    }
+
+    if (!WindowsTrayManager::instance().initialize()) {
+        QMessageBox::critical(nullptr, "Error",
+            "Initialization failed");
         return 1;
     }
 
